03cd3011602fa10e79335d51d30cb6b5ec3ccd27
Richard <pfrichard95@gmail.com>
2023-03-10 19:11:39 +0100

    test conf

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ print('test')
 print('diff source')
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print('conflict')
 
 
 # Press the green button in the gutter to run the script.
