4b4763326473f206ff48a98a6d57b168487b15b9
Richard <pfrichard95@gmail.com>
2023-03-22 19:26:40 +0100

    updt

--- Simple_neural/smpl_neural.py
+++ Simple_neural/smpl_neural.py
@@ -3,33 +3,22 @@ import torch.nn as nn
 import torch.optim as optim
 import numpy as np
 
-#dataset = [ ([1.0,2.0,3.0,4.0],1.0) , ([4.0,3.0,2.0,1.0],0.0) ]
 
-dataset = [
-    ([0, 1, 0,
-      0, 1, 0,
-      0, 1, 0,],1),
-    ([1, 1, 0,
-0, 1, 0,
-1, 1, 1],2),
-    ([1, 1, 1,
-    0, 0, 1,
-    1, 1, 1,],3),
-    ([0, 1, 0,
-1, 1, 0,
-0, 1, 0],4),
-]
+#input : distance between 2 pipes bird position
+# e.g : [birdx, birdy, pipe1_x, pipe1_y, pipe2_x, pipe2_y] : 6
+
+#output : jump or no jump
+# e.g [jump] 1
+dataset = [([1,2,3,4,5,6],0)]
 
 class Simple_Net(nn.Module):
     def __init__(self):
         super(Simple_Net, self).__init__()
-        self.fc1 = nn.Linear(9, 11)
-        self.fc2 = nn.Linear(11, 1)
+        self.fc1 = nn.Linear(6, 7)
+        self.fc2 = nn.Linear(7, 1)
         #self.relu = nn.Sigmoid()
 
     def forward(self, x):
-        #x = x.view(-1, 4)
-        #x = self.relu(self.fc1(x))
         x = self.fc1(x)
         x = self.fc2(x)
         return x
@@ -54,8 +43,6 @@ for epoch in range(600):
         # to fix dimensionality conflict add it to a list
         tar_tensor = torch.tensor([tar])
         optimizer.zero_grad()
-
-
         outputs = net(input_tensor)
         #print(f'{input_tensor.shape},{tar_tensor.shape}, {outputs.shape}')
         loss = criterion(outputs, tar_tensor)
@@ -68,9 +55,6 @@ for epoch in range(600):
 
 
 with torch.no_grad():
-    print('test cycle: ')
-    joker = [1.0,1.0,0.0,1.0,0.0,0.0,1.0,0.0,0.0]
-    joker_tensor = torch.tensor(joker)
     for inp, tar in dataset:
         inp = [float(i) for i in inp]
         tar = float(tar)
@@ -79,4 +63,3 @@ with torch.no_grad():
         tar_tensor = torch.tensor([tar])
         outputs = net(input_tensor)
         print(f'For {inp} we expect {tar} and received {outputs.data}')
-    print(f'For joker {inp}  and received {net(joker_tensor).data}')
